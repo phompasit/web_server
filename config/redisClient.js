d9d42e150998b277e03aa61126668a29755cb5d3
phompasith sisanonh <98532227+phompasithsisanonh@users.noreply.github.com>
2025-10-08 22:46:08 +0700

    ss

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,9 +1,19 @@
-const Redis =require("ioredis")
+// const Redis =require("ioredis")
+
+// const redis = new Redis({
+//   host: process.env.REDIS_HOST || "redis", // ต้องเป็นชื่อ service ใน docker-compose
+//   port: process.env.REDIS_PORT || 6379,
+// });
+
+// redis.on("connect", () => console.log("Redis connected"));
+// redis.on("error", (err) => console.error("Redis error:", err));
+
+const { Redis }= require('@upstash/redis')
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || "redis", // ต้องเป็นชื่อ service ใน docker-compose
-  port: process.env.REDIS_PORT || 6379,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
-redis.on("connect", () => console.log("Redis connected"));
-redis.on("error", (err) => console.error("Redis error:", err));
+redis.set("foo", "bar").then(() => console.log("Set success"));
+redis.get("foo").then((val) => console.log("Value:", val));
